ffffe39b889a218f111369eadf36150f716f41c3
ertugdlk <ertgdlk@gmail.com>
2021-12-28 16:01:18 +0300

    backend url changed HOTFIX#1

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 const baseUrl =
-    "http://http://ec2-18-159-132-210.eu-central-1.compute.amazonaws.com:5000/"
+    "http://ec2-18-159-132-210.eu-central-1.compute.amazonaws.com:5000/"
 
 export { baseUrl }
